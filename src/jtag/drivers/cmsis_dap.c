c95792d0a3ed86d325ebf28a50864ff6e366c5e1
kong lei <1063299919@qq.com>
2026-06-21 13:02:15 +0800

    aaa

--- src/jtag/drivers/cmsis_dap.c
+++ src/jtag/drivers/cmsis_dap.c
@@ -1345,6 +1345,20 @@ static int cmsis_dap_init(void)
 	if (retval != ERROR_OK)
 		return retval;
 
+	{
+		extern uint32_t ref_mv;
+		if (ref_mv>3300) ref_mv = 3300;
+		if (ref_mv<1500) ref_mv = 1500;
+		uint8_t *command = cmsis_dap_handle->command;
+
+		command[0] = 0x9D;
+		command[1] = ref_mv/100;
+		retval = cmsis_dap_xfer(cmsis_dap_handle, 2);
+		if (retval != ERROR_OK || cmsis_dap_handle->response[1] != DAP_OK)
+			goto init_err;
+		LOG_INFO("CMSIS-DAP: set vref %d mv", ref_mv);
+	}
+
 	if (swd_mode) {
 		retval = cmsis_dap_swd_open();
 		if (retval != ERROR_OK)
@@ -1458,20 +1472,6 @@ static int cmsis_dap_init(void)
 				goto init_err;
 			LOG_INFO("Connecting under reset");
 		}
-	}
-
-	{
-		extern uint32_t ref_mv;
-		if (ref_mv>3300) ref_mv = 3300;
-		if (ref_mv<1500) ref_mv = 1500;
-		uint8_t *command = cmsis_dap_handle->command;
-
-		command[0] = 0x9D;
-		command[1] = ref_mv/100;
-		retval = cmsis_dap_xfer(cmsis_dap_handle, 2);
-		if (retval != ERROR_OK || cmsis_dap_handle->response[1] != DAP_OK)
-			goto init_err;
-		LOG_INFO("CMSIS-DAP: set vref %d mv", ref_mv);
 	}
 
 	extern bool enable_cjtag;
